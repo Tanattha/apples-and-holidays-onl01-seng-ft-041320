8cf4f370791a2882f9ea1e2cac0cde453cd6e3ca
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-18 13:20:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season,holidays|
     holidays.collect do |holiday,supplies|
       if supplies.include? ("BBQ")
-     holiday.map {|check| check.delete_if { |k,v| v.nil? } 
+     holiday.delete_if { |k,v| v.nil? } 
      
   end
    end
